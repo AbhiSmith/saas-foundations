be6db68b08791e86417d37f84ab7bf3ed064fb88
AbhiSmith <abhishekv4004@gamil.com>
2024-06-09 22:33:54 +0530

    update setting

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,7 +25,17 @@ SECRET_KEY = 'django-insecure-#7n&(vbz7^oe*rg=oxrg=d#)3qgs+scby6p(34r4d-0$1$y_kw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
